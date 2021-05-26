98dc53430b9bf2d96ce9431ea20d14439118161e
Apocalypse <aduzgun99@gmail.com>
2021-05-26 04:14:52 +0300

    Initial commit

---
+++ ChatWebParent/ChatBackEnd/src/main/java/com/chat/admin/ChatbackendApplication.java
@@ -0,0 +1,13 @@
+package com.chat.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatbackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatbackendApplication.class, args);
+    }
+
+}
